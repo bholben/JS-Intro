862fd4eb0d500bd8d478e8bfe527a6090f7d0a7b
Bob Holben <bholben@gmail.com>
2015-01-13 15:43:42 -0500

    Initial commit with Answers in main.js file

---
+++ main.js
@@ -0,0 +1,42 @@
+var name;
+// Q: What does name equal right now?
+// A: undefined
+
+name = "Tim";
+// Q: What is `name` set to right now?
+// A: "Tim"
+
+name = "Bill";
+// Q: What is `name` set to right now?
+// A: "Bill"
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: "Hello, how are you?"
+
+var high = 50;
+var low  = 10;
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: 40
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: 45
+
+// Create a variable to calculate my age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+
+var age = today - born;  // <-- I added this.
+
+// Store some information following in variables.
+var yourname;
+var myname;
+
+yourname = "Bob";  // <-- I added this.
+myname = "Tim";  // <-- I added this.
+
+var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
